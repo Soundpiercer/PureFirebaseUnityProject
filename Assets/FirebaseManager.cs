c8789d3aaf585b942f0d9364c26e82ea76f1da4a
Sound piercer Official <soundpiercer@gmail.com>
2019-08-17 22:27:49 +0900

    Firebase Cloud Messaing (FCM) Import : faced DLLException issued, solved using Force Resolve

---
+++ Assets/FirebaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    private void Start()
+    {
+        FirebaseCloudMessagingInit();
+    }
+
+    #region Firebase Cloud Messaging (FCM)
+    private void FirebaseCloudMessagingInit()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+    #endregion
+}
